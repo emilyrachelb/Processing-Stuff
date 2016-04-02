0335a3b2c62bb3250e5c8b05b8785797471cee19
Chi Ho Belnavis <belnavischiho@gmail.com>
2016-04-01 23:42:15 -0400

    such meme, so dank

--- dankestMemeEver/application.linux32/source/dankestMemeEver.pde
+++ dankestMemeEver/application.linux32/source/dankestMemeEver.pde
@@ -1,14 +1,19 @@
-//Import sound and animation libraries
-import gifAnimation.*;
+//Import sound libraries
+
 import ddf.minim.*;
 
 //Create photo elements
 PImage triangle;
+PImage triangle2;
+PImage spaceweed1;
+PImage spaceweed2;
+PImage spaceweed3;
+PImage spaceweed4;
 PImage mlg;
 PImage doritos;
 PImage[] animation;
 
-Gif gif;
+
 Minim minim;
 AudioPlayer player;
 AudioInput input;
@@ -19,28 +24,33 @@ float programTimer = 2020;
 
 void setup() {
   size(1280, 700);
-  frameRate(60);
+  frameRate(420);
 
   minim = new Minim(this);
 
   player = minim.loadFile("X-Files TRAP REMIX.mp3");
-
+  
+  PFont meme;
+  meme = loadFont("memefont.vlw");
+  textFont(meme, 48);
+  
   //Load photo elements
   triangle = loadImage("triangles.png");
-  mlg = loadImage("mlg.png");
-  doritos = loadImage("doritos.png");
-
-  //Load Snoop Dogg GIF
-  gif = new Gif(this, "smoke weed everyday.gif");
-  gif.loop();
+  triangle2 = loadImage("triangles.png");
+  spaceweed1 = loadImage("spaceweed.png");
+  spaceweed2 = loadImage("spaceweed.png");
+  spaceweed3 = loadImage("spaceweed.png");
+  spaceweed4 = loadImage("spaceweed.png");
+  //mlg = loadImage("mlg.png");
+  //doritos = loadImage("doritos.png");
 }
 
 
 void draw() {
   float randomColourR = random(0, 255);
   float randomColourG = random(0, 255);
-  float randomColourB = random(0, 255);
-
+  float randomColourB = random(0, 255); 
+  
   background(randomColourR, randomColourG, randomColourB);
 
   player.play();
@@ -50,11 +60,13 @@ void draw() {
   triangleCounter = triangleCounter + 60;
   mlgCounter = mlgCounter + 35;
 
+  text("ted cruz is the zodiac killer", 350, 250);
+  text("ted cruz ate my son", 400, 350);
+  text("blaze weed m9", 450, 450);
   imageMode(CORNER);
-  image(gif, 920, 450);
 
   imageMode(CENTER);
-  pushMatrix();
+  /*pushMatrix();
   translate(125, 105);
   rotate(mlgCounter * PI / 360);
   image(mlg, 0, 0);
@@ -65,12 +77,43 @@ void draw() {
   rotate(mlgCounter * PI / 360);
   image(doritos, 0, 0, 167, 241.5);
   popMatrix();
-
+*/
   pushMatrix();
-  translate(640, 350);
+  translate(10, 350);
   rotate(triangleCounter*PI/360);
   image(triangle, 0, 0);
   popMatrix();
+  
+  pushMatrix();
+  translate(1270, 350);
+  rotate(triangleCounter*PI/360);
+  image(triangle2, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(1270, 50);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(1270, 550);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(10, 50);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(10, 550);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
   if (programTimer <= 0) {
     noLoop();
     exit();
@@ -83,4 +126,3 @@ void stop() {
   exit();
   super.exit();
 }
-
--- dankestMemeEver/application.windows32/source/dankestMemeEver.java
+++ dankestMemeEver/application.windows32/source/dankestMemeEver.java
@@ -3,7 +3,6 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
-import gifAnimation.*; 
 import ddf.minim.*; 
 
 import java.util.HashMap; 
@@ -17,17 +16,22 @@ import java.io.IOException;
 
 public class dankestMemeEver extends PApplet {
 
-//Import sound and animation libraries
+//Import sound libraries
 
 
 
 //Create photo elements
 PImage triangle;
+PImage triangle2;
+PImage spaceweed1;
+PImage spaceweed2;
+PImage spaceweed3;
+PImage spaceweed4;
 PImage mlg;
 PImage doritos;
 PImage[] animation;
 
-Gif gif;
+
 Minim minim;
 AudioPlayer player;
 AudioInput input;
@@ -37,29 +41,34 @@ float mlgCounter = 0.0f; //MLG Logo photo rotation speed
 float programTimer = 2020;
 
 public void setup() {
-  size(1280, 700);
-  frameRate(60);
+  
+  frameRate(420);
 
   minim = new Minim(this);
 
   player = minim.loadFile("X-Files TRAP REMIX.mp3");
-
+  
+  PFont meme;
+  meme = loadFont("memefont.vlw");
+  textFont(meme, 48);
+  
   //Load photo elements
   triangle = loadImage("triangles.png");
-  mlg = loadImage("mlg.png");
-  doritos = loadImage("doritos.png");
-
-  //Load Snoop Dogg GIF
-  gif = new Gif(this, "smoke weed everyday.gif");
-  gif.loop();
+  triangle2 = loadImage("triangles.png");
+  spaceweed1 = loadImage("spaceweed.png");
+  spaceweed2 = loadImage("spaceweed.png");
+  spaceweed3 = loadImage("spaceweed.png");
+  spaceweed4 = loadImage("spaceweed.png");
+  //mlg = loadImage("mlg.png");
+  //doritos = loadImage("doritos.png");
 }
 
 
 public void draw() {
   float randomColourR = random(0, 255);
   float randomColourG = random(0, 255);
-  float randomColourB = random(0, 255);
-
+  float randomColourB = random(0, 255); 
+  
   background(randomColourR, randomColourG, randomColourB);
 
   player.play();
@@ -69,11 +78,13 @@ public void draw() {
   triangleCounter = triangleCounter + 60;
   mlgCounter = mlgCounter + 35;
 
+  text("ted cruz is the zodiac killer", 350, 250);
+  text("ted cruz ate my son", 400, 350);
+  text("blaze weed m9", 450, 450);
   imageMode(CORNER);
-  image(gif, 920, 450);
 
   imageMode(CENTER);
-  pushMatrix();
+  /*pushMatrix();
   translate(125, 105);
   rotate(mlgCounter * PI / 360);
   image(mlg, 0, 0);
@@ -82,14 +93,45 @@ public void draw() {
   pushMatrix();
   translate(1125, 105);
   rotate(mlgCounter * PI / 360);
-  image(doritos, 0, 0, 167, 241.5f);
+  image(doritos, 0, 0, 167, 241.5);
   popMatrix();
-
+*/
   pushMatrix();
-  translate(640, 350);
+  translate(10, 350);
   rotate(triangleCounter*PI/360);
   image(triangle, 0, 0);
   popMatrix();
+  
+  pushMatrix();
+  translate(1270, 350);
+  rotate(triangleCounter*PI/360);
+  image(triangle2, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(1270, 50);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(1270, 550);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(10, 50);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
+  pushMatrix();
+  translate(10, 550);
+  rotate(triangleCounter*PI/360);
+  image(spaceweed1, 0, 0);
+  popMatrix();
+  
   if (programTimer <= 0) {
     noLoop();
     exit();
@@ -102,7 +144,7 @@ public void stop() {
   exit();
   super.exit();
 }
-
+  public void settings() {  size(1280, 700); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "dankestMemeEver" };
     if (passedArgs != null) {
